1194c83b06c37201c3bb87cc289038ccdd17cc4b
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-12-01 07:03:45 -0600

    Update object_values.js
     how to navigate in an object key and object values

--- object_values.js
+++ object_values.js
@@ -9,3 +9,17 @@ console.log(Object.values(object1));
 for (var j of Object.values(object1)){
   alert(j);
 }
+
+// how to navigate in an object key and object values
+
+var obj = {1:"a",2:"b",3:"c",4:"d",5:"e"};
+for(var i in obj){
+  console.log("keys :" + " " +i);
+  console.log("values :" + " " +obj[i]);
+}
+
+var keys = Object.keys(obj);
+console.log("recuperation : " + " " + keys[0]);
+var size = keys.length;
+console.log("size : " + " " + size);
+console.log("recuperation : " + " " + keys[4]);
